81779436b943a8ed6d5191dd61061dbb182442db
Ryhan <ryhanramos@gmail.com>
2021-01-10 16:54:00 -0800

    finished sticky header

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -16,8 +16,7 @@ class RevealOnScroll {
         window.addEventListener("resize", debounce(()=> {
             console.log("Resize just ran")
             this.browserHeight = window.innerHeight
-        }, 333
-        ))
+        }, 333))
     }
 
     calcCaller() {
