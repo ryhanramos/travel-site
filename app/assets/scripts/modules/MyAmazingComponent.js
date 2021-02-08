012e6faac781ddb1b60d52ad4a870390187c2d0d
Ryhan <ryhanramos@gmail.com>
2021-02-08 13:15:26 -0800

    added wrapper class to react section

--- app/assets/scripts/modules/MyAmazingComponent.js
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function MyAmazingComponent() {
     return (
-        <div>
+        <div className="wrapper">
             <h1 className="section-title section-title--blue">This is my Amazing React Component</h1>
             <p>React is great, grass is blue, grass is green</p>
         </div>
