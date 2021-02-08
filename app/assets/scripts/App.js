d6bff602b6f80a928a9c0dc7b9dc8b2a5e257aa3
Ryhan <ryhanramos@gmail.com>
2021-02-08 11:26:20 -0800

    Added CSS Grid to Layout

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,6 +5,16 @@ import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 import ClientArea from './modules/ClientArea';
 
+//React code goes here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//Import React components
+
+import MyAmazingComponent from './modules/MyAmazingComponent'
+
+ReactDOM.render(<MyAmazingComponent />, document.querySelector("#my-react-example"))
+
 new ClientArea()
 new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
---
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MyAmazingComponent() {
+    return (
+        <div>
+            <h1 className="section-title section-title--blue">This is my Amazing React Component</h1>
+            <p>React is great, grass is blue, grass is green</p>
+        </div>
+    )
+}
+
+export default MyAmazingComponent
